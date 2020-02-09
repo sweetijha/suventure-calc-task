da28951ae52dce9d9a7edbacf18a3d6191c86f73
Sweeti <sweetijha11@gmail.com>
2020-02-09 16:01:04 +0530

    initial commit

--- calc/src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import Calc from "./Calc";
-import "./style.css";
-
-const wrapper = document.getElementById("container");
-console.log("wrapper.", wrapper);
-wrapper ? ReactDOM.render(<Calc />, wrapper) : false;
